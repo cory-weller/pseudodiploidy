074b4f940902751308d26ee822be352c37d4ac23
Cory Weller <cory.a.weller@gmail.com>
2022-05-18 15:09:29 -0400

    fix classifier

--- src/analyze_sim_reads.py
+++ src/analyze_sim_reads.py
@@ -6,11 +6,11 @@ import logging
 import contextlib
 import itertools
 import yaml
+import time
 import regex as re
 from yaml.loader import SafeLoader
 
-#import inspect
-#from Bio import AlignIO
+logger = None
 
 def yaml_load(yamlfilename):
     '''Loads a .yaml file and returns the contents as a dictionary'''
@@ -49,19 +49,6 @@ def reverse_complement(seq):
     return reverse_seq
 
 
-# def split_seq(seq, geneName, regexLeft, regexRight):
-#     ''' Trims sequence before and after defined constant region regex patterns'''
-#     # Trim left constant sequence
-#     try:
-#         match_L = re.search(regexLeft, seq).captures()[0]
-#         before_match_L, after_match_L = seq.split(match_L)
-#         match_R = re.search(regexRight, after_match_L).captures()[0]
-#         before_match_R, after_match_R = after_match_L.split(match_R)
-#     except AttributeError:
-#         return None
-#     return([geneName, seq, before_match_L, match_L, before_match_R, match_R, after_match_R])
-
-
 def check_pythonpath():
     '''Confirms current directory is in $PYTHONPATH'''
     try:
@@ -76,22 +63,23 @@ def check_pythonpath():
         )
 
 
-def match_sequence_perfect(seq, amplicons):
-    '''returns array for whether or not the given seqs match any patterns'''
-    output = []
-    for i in amplicons:
-        if re.search(i.regex_pattern1, seq):
-            output.append(i.name)
-    if len(output) > 0:
-        return output
-    return None
+# def match_sequence_perfect(seq, amplicons):
+#     '''returns array for whether or not the given seqs match any patterns'''
+#     output = []
+#     for i in amplicons:
+#         if re.search(i.regex_pattern1, seq):
+#             output.append(i.name)
+#     if len(output) > 0:
+#         return output
+#     return None
 
 def match_sequence_fuzzy(seq, amplicons):
     '''returns array for whether or not the given seqs match any patterns'''
-    for i in amplicons:
-        if re.search(i.regex_pattern2, seq):
-            return i.name
-    return ['nomatch']
+    for amplicon in amplicons:
+        for pattern in amplicon.patterns:
+            if re.search(pattern, seq):
+                return amplicon.name
+    return None
 
 
 class Amplicon:
@@ -112,84 +100,86 @@ class Amplicon:
         self.wt = wt
         self.edit = edit
         self.seq = self.upstream[pad_left : -pad_right]
-        self.regex_pattern1 = re.compile(fr"({self.seq})")
-        self.regex_pattern2 = re.compile(fr"({self.seq}){{e<={max_errors}}}")
+        self.patterns = [re.compile(fr"({self.seq}){{e<={error}}}") for error in max_errors]
 
-if __name__ == '__main__':
-    # Load config and 
 
-    CONFIG = yaml_load('config.yaml')
-    DEBUG_ARGS = CONFIG['debug_args']
+def main():
+    '''Main'''
+    startTime = time.time()
+    config = yaml_load('config.yaml')
 
     # Initialize logging
-    LOGGING_LEVEL = CONFIG['logging']
+    global logger
     logger = logging.getLogger()
     logger.setLevel(logging.NOTSET)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(level=LOGGING_LEVEL)
+    console_handler.setLevel(level=config['logging'])
     console_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
     logger.addHandler(console_handler)
 
     # Parse args
     myargs = sys.argv
     if len(myargs) == 1:       # if no args supplied, e.g. debuging
-        print('\n'.join(['running with debug arguments:'] + DEBUG_ARGS))
-        myargs = DEBUG_ARGS
+        myargs = config['debug_args']
+        logger.debug("Running with debug arguments defined in config.yaml")
+    logger.info("Arguments: %s", myargs)
 
     fastq_name = myargs[1]        # /path/to/file/BATCH_SAMPLENAME.assembled.fastq
     logger.info("fastq input filename: %s", fastq_name)
     # filestem formatted/parsed with hyphen delimiter, as BATCH-SAMPLE.assembled.fastq
-    filestem = os.path.basename(fastq_name).split('.assembled.fastq')[0] 
+    filestem = os.path.basename(fastq_name).split('.assembled.fastq')[0]
     logger.info("filestem: %s", filestem)
     batch, samplename = filestem.split('-')
     logger.info("batch: %s", batch)
     logger.info("samplename: %s", samplename)
 
     # Initialize amplicons
-    MISMATCH_TOLERANCE = float(CONFIG['pct_mismatch_tolerance'])
-    gene_names = list(CONFIG['amplicons'].keys())
+    mismatch_tolerances = config['pct_mismatch_tolerances']
+    logger.info("Mismatch Tolerances: %s", mismatch_tolerances)
+    gene_names = list(config['amplicons'].keys())
     amplicons = []
     for gene in gene_names:
         amplicon = Amplicon(gene,
-                    CONFIG['amplicons'][gene]['upstream'],
-                    CONFIG['amplicons'][gene]['downstream'],
-                    CONFIG['amplicons'][gene]['wt'],
-                    CONFIG['amplicons'][gene]['edit'],
-                    CONFIG['pad_left'],
-                    CONFIG['pad_right'],
-                    int(len(CONFIG['amplicons'][gene]['upstream'])* MISMATCH_TOLERANCE)
+                    config['amplicons'][gene]['upstream'],
+                    config['amplicons'][gene]['downstream'],
+                    config['amplicons'][gene]['wt'],
+                    config['amplicons'][gene]['edit'],
+                    config['pad_left'],
+                    config['pad_right'],
+                    {int(len(config['amplicons'][gene]['upstream'])*i) for i in mismatch_tolerances}
         )
         amplicons.append(amplicon)
 
     for amplicon in amplicons:
         logger.info("adding amplicon: %s", amplicon.name)
-        logger.info("adding perfect match pattern: %s", amplicon.regex_pattern1)
-        logger.info("adding fuzzy match pattern: %s", amplicon.regex_pattern2)
+        logger.info("adding perfect match pattern: %s", amplicon.patterns)
 
     # Evaluate reads
 
     with contextlib.ExitStack() as stack, \
     open(fastq_name, 'r', encoding='utf-8') as infile:
         files = {}
-        unmatched = []
         for gene in gene_names +  ['nomatch']:
-            filename = f'data/processed/{samplename}-{gene}.txt'
+            filename = f'data/processed/{batch}-{samplename}-{gene}.counts'
             files[gene] = stack.enter_context(open(filename, 'w', encoding='utf-8'))
         # Iterate over fastq lines for perfect matches
         logger.info("iterating over raw reads")
-        for line in itertools.zip_longest(*[infile]*4):
-            seq = line[1].strip()
-            matches = match_sequence_perfect(seq, amplicons)
-            if matches is None:
-                unmatched.append(seq)
-            else:
-                for match in matches:
-                    print(seq, file=files[match])
-        # Iterate over unmatched sequences with fuzzy tolerance
-        logger.info("iterating over %s unmatched reads with fuzzy tolerance", len(unmatched))
-        for seq in unmatched:
-            matches = match_sequence_fuzzy(seq, amplicons)
-            for match in matches:
-                print(match, file=files[match])
-
+        i = 0
+        # for line in itertools.zip_longest(*[infile]*4):  # for iterating over fastq
+        for line in infile:     # for iterating over table of <count> <read>
+            i += 1
+            if(i % 10000) == 0:
+                logger.info("%s reads done", i)
+            count, seq = line.strip().split()
+            match = match_sequence_fuzzy(seq, amplicons)
+            # here
+            if match is None:
+                match = 'nomatch'
+            print(f"{count}\t{seq}", file=files[match])
+    run_time = (time.time() - startTime)
+    logger.info("Total runtime: %s", run_time)
     sys.exit()
+
+
+if __name__ == '__main__':
+    main()
